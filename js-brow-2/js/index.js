f2279f2367a100b12aa13720351428d5b8fb6e58
Federico <federicoc03@gmail.com>
2023-03-28 14:46:05 -0500

    Falta estilo del checkbox y el hr, ya mejor hacerle a JS

--- js-brow-2/js/index.js
+++ js-brow-2/js/index.js
@@ -6,17 +6,23 @@ add.addEventListener('click', function () {
     //Estuve como 1 hora sin darme cuenta que por ser de tyype submit
     //el boton siempre recargaba la pag
     event.preventDefault()
-    let addTask = tarea.value;    
+    let addTask = tarea.value;
+    let li = document.createElement('li');
     let label = document.createElement('label');
+        label.setAttribute('id', 'labelCheckbox')
     let checkbox = document.createElement('input');
         checkbox.setAttribute('type', 'checkbox')
+        checkbox.setAttribute('id', 'check');
 
     let text = document.createTextNode(addTask);
+    let line = document.createElement('hr');
 
+    list.appendChild(li);
     list.appendChild(label);
 
     label.appendChild(text);
     label.appendChild(checkbox);
+    list.appendChild(line);
     
     
 });
