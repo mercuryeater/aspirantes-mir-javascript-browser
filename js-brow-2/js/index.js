27574f536c774412c5751549cffcc6c06dc02b61
Federico <federicoc03@gmail.com>
2023-03-30 09:58:32 -0500

    Solucion a los ejercicios de js en el navegador II

--- js-brow-2/js/index.js
+++ js-brow-2/js/index.js
@@ -1,63 +1,72 @@
 const add = document.querySelector("#add");
 const list = document.querySelector("#list");
 const tarea = document.querySelector("#tarea");
-
-add.addEventListener('click', function () {
-    //Estuve como 1 hora sin darme cuenta que por ser de tyype submit
-    //el boton siempre recargaba la pag
-    event.preventDefault()
-    let addTask = tarea.value;
-    let li = document.createElement('li');
-    let label = document.createElement('label');
-        label.setAttribute('id', 'labelCheckbox')
-    let checkbox = document.createElement('input');
-        checkbox.setAttribute('type', 'checkbox')
-        checkbox.setAttribute('id', 'check');
-
-    let text = document.createTextNode(addTask);
-    let line = document.createElement('hr');
-
-    list.appendChild(li);
-    list.appendChild(label);
-
-    label.appendChild(text);
-    label.appendChild(checkbox);
-    list.appendChild(line);
-    
-    
-});
-
-/*
-CODIGO PROFESORA PARA COMPLETAR Y ADAPTAR
-
-const form = document.querySelector('form')
-const inputTask = document.getElementById('new-task')
-const listItems = document.querySelector('.list-items')
+const inputBox = document.querySelector("#inputBox");
+const deleteCompleted = document.getElementById("delete");
+let uniqueId = 1;
 
 function handleSubmit(event) {
   event.preventDefault();
-  const task = inputTask.value;
-  // Creamos un fragmento para luego agregar al documento
-  const fragment = document.createDocumentFragment();
-  const inputText = document.createElement("input")
-  inputText.setAttribute('type', 'checkbox')
-  inputText.setAttribute('id', task)
-  const label = document.createElement("label")
-  label.setAttribute('for', task)
-  label.innerHTML = task
-  // falta agregar el atributo for con el mismo nombre del id del checkbox
-  // Falta hacer el appendChild del label que contiene el task
-const listItem = fragment
-  .appendChild(document.createElement("li"))
-  .appendChild(label)
-  .appendChild(inputText);
-  
-  // hacemos appendChild a la clase list-items
-  listItems.appendChild(fragment);
+  let addTask = tarea.value;
+  let li = document.createElement("li");
+  let label = document.createElement("label");
+  label.setAttribute("id", uniqueId++);
+  label.setAttribute("class", "non-completed");
+  label.setAttribute("title", addTask);
+  label.setAttribute("completed", false);
+
+  let checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.setAttribute("id", "check");
+
+  let text = document.createTextNode(addTask);
+  let line = document.createElement("hr");
+
+  list.appendChild(li);
+  li.appendChild(label);
+
+  label.appendChild(text);
+  label.appendChild(checkbox);
+  li.appendChild(line);
 }
 
-form.addEventListener('submit', handleSubmit)
-*/
+function handleCheckTask(event) {
+  // Obtenemos el checkbox que se ha cambiado
+  const checkbox = event.target;
+  // Verificamos que sea un input de tipo checkbox, la propiedad tagName devuelve
+  // el nombre del tag en mayusculas y por eso toca poner INPUT
+  if (checkbox.tagName === "INPUT" && checkbox.type === "checkbox") {
+    // Obtenemos el label asociado al checkbox
+    const label = checkbox.parentNode;
+    if (checkbox.checked) {
+      label.setAttribute("completed", true);
+      label.setAttribute("class", "is-completed");
+    } else {
+      label.setAttribute("completed", false);
+      label.setAttribute("class", "non-completed");
+    }
+  }
+}
 
+function handleDelete(event) {
+  event.preventDefault();
+  const labels = document.querySelectorAll("label");
+  const completedTasks = [];
+  for (let i = 0; i < labels.length; i++) {
+    const label = labels[i];
+    if (label.getAttribute("completed") === "true") {
+      completedTasks.push(label);
+      console.log(completedTasks);
+    }
+  }
+  for (let i = 0; i < completedTasks.length; i++) {
+    const label = completedTasks[i];
+    label.parentNode.remove();
+  }
+}
 
+deleteCompleted.addEventListener("click", handleDelete);
 
+list.addEventListener("change", handleCheckTask);
+
+inputBox.addEventListener("submit", handleSubmit);
